0e136ae13e9600866757b42baf1b255b4eff5845
Brianna Bell <briannabell84@gmail.com>
2024-12-13 14:33:18 -0800

    Added menu and readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Cards of Personality Importer
- Deletes all template white and black cards, then adds new cards from a .csv file
+
+This is a tool for https://www.cardsofpersonality.com/ which is an online cards against humanity clone. Users can create their own custom decks, but deleting all of the template cards and typing in all of the new prompts can take a while. This script can delete all of the existing cards and import your cards from a text file. 
+Links to the code for Cards of Personality: 
+https://github.com/sdennett55/cards-of-personality-frontend
+https://github.com/sdennett55/cards-of-personality-backend
+
+
+# To Install
+You'll need:
+*  VS Code with a python interpreter
+* Selenium 
+* Firefox with selenium 
+* 2 text files with the black card phrases and white card phrases listed
--- cop_import.py
+++ cop_import.py
@@ -1,3 +1,6 @@
+# Made by Brianna Bell
+# 12/13/2024
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
@@ -7,11 +10,15 @@ from selenium.webdriver.common.alert import Alert
 from selenium.common.exceptions import NoAlertPresentException
 import csv
 
+url = input('Welcome to the python cards of personality importer. Please enter the full URL to the edit page of your set of cards: ')
+
+# should be formatted like https://www.cardsofpersonality.com/edit-deck/DECK-NAME?secret=XYZ
+
 #Set up Firefox WebDriver
 options = Options()
 options.binary_location = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
 driver = webdriver.Firefox()
-driver.get('https://www.cardsofpersonality.com/edit-deck/testubg-8936?secret=6758d9c8fb144d00392d147e')
+driver.get(url)
 
 # Wait for the table to appear on the page/for the page to fully load
 try:
@@ -23,78 +30,83 @@ try:
 except TimeoutError:
     print("Timed out waiting for table to load.")
 
+deleteExistingCards = input('Do you want to delete all existing cards? (it will take a while) y/n ')
+if(deleteExistingCards.lower() == 'y'):
 
-# check for existing white cards
-# numWhite = driver.find_element(By.CLASS_NAME, 'sc-pjHjD.CNlcm')
-# if (int(numWhite.text) > 0):
+    #check for existing white cards
+    numWhite = driver.find_element(By.CLASS_NAME, 'sc-pjHjD.CNlcm')
+    if (int(numWhite.text) > 0):
 
-#     # white cards - expand button
-#     expandbut = driver.find_element(By.CLASS_NAME, 'sc-prorn.fpBoEr')
-#     expandbut.click()
+        # white cards - expand button
+        expandbut = driver.find_element(By.CLASS_NAME, 'sc-prorn.fpBoEr')
+        expandbut.click()
 
-#     # white cards - get all current cards and delete them one by one
-#     buttons = driver.find_elements(By.CLASS_NAME, 'sc-pbYBj.fWqrEZ')
+        # white cards - get all current cards and delete them one by one
+        buttons = driver.find_elements(By.CLASS_NAME, 'sc-pbYBj.fWqrEZ')
 
-#     deletedcards = 0
+        deletedcards = 0
 
-#     for pushy in buttons:
-#         WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable(pushy)).click()
-#         #pushy.click()
-#         alert = Alert(driver)
-#         try:
-#             # Try to switch to the alert
-#             alert = Alert(driver)
-#             # If no exception is raised, the alert is present
-#             alert.accept()  # Accepts the alert
-#             #print("Alert accepted")
-#         except NoAlertPresentException:
-#             num = 567
-#         deletedcards += 1
-#         print('{} white cards deleted'.format(deletedcards))
+        for pushy in buttons:
+            WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable(pushy)).click()
+            #pushy.click()
+            alert = Alert(driver)
+            try:
+                # Try to switch to the alert
+                alert = Alert(driver)
+                # If no exception is raised, the alert is present
+                alert.accept()  # Accepts the alert
+                #print("Alert accepted")
+            except NoAlertPresentException:
+                num = 567
+            deletedcards += 1
+            print('{} white cards deleted'.format(deletedcards))
 
-# # check for existing black cards
-# numBlack = driver.find_element(By.CLASS_NAME, 'sc-pjHjD.CNlcm')
-# if (int(numBlack.text) > 0):
-#     # black cards - expand button
-#     expandbut = driver.find_element(By.CLASS_NAME, 'sc-prorn.fpBoEr')
-#     expandbut.click()
+    # check for existing black cards
+    numBlack = driver.find_element(By.CLASS_NAME, 'sc-pjHjD.CNlcm')
+    if (int(numBlack.text) > 0):
+        # black cards - expand button
+        expandbut = driver.find_element(By.CLASS_NAME, 'sc-prorn.fpBoEr')
+        expandbut.click()
 
-#     # black cards - get all current cards and delete them one by one
-#     buttons = driver.find_elements(By.CLASS_NAME, 'sc-pbYBj.fWqrEZ')
+        # black cards - get all current cards and delete them one by one
+        buttons = driver.find_elements(By.CLASS_NAME, 'sc-pbYBj.fWqrEZ')
 
-#     deletedcards = 0
+        deletedcards = 0
 
-#     for pushy in buttons:
-#         WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable(pushy)).click()
-#         #pushy.click()
-#         alert = Alert(driver)
-#         try:
-#             # Try to switch to the alert
-#             alert = Alert(driver)
-#             # If no exception is raised, the alert is present
-#             alert.accept()  # Accepts the alert
-#             #print("Alert accepted")
-#         except NoAlertPresentException:
-#             num = 567
-#         deletedcards += 1
-#         print('{} black cards deleted'.format(deletedcards))
+        for pushy in buttons:
+            WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable(pushy)).click()
+            #pushy.click()
+            alert = Alert(driver)
+            try:
+                # Try to switch to the alert
+                alert = Alert(driver)
+                # If no exception is raised, the alert is present
+                alert.accept()  # Accepts the alert
+                #print("Alert accepted")
+            except NoAlertPresentException:
+                num = 567
+            deletedcards += 1
+            print('{} black cards deleted'.format(deletedcards))
 
+whiteCardFile = input('Enter the path to the white card text file: ')
 
-# with open('baiwhite.tsv', newline='', encoding="utf8") as csvfile:
+with open(whiteCardFile, newline='', encoding="utf8") as csvfile:
 
-#    reader2 = csv.reader(csvfile, delimiter='\n', quotechar='|')
+   reader2 = csv.reader(csvfile, delimiter='\n', quotechar='|')
 
-#    for row in reader2:
+   for row in reader2:
         
-#         rowStr = ''.join(row)
-#         rowNoSpaces = rowStr.strip(' \t\n\r')
-#         rowDone = rowNoSpaces.replace('\"\"','\"')
-#         print(rowDone)
+        rowStr = ''.join(row)
+        rowNoSpaces = rowStr.strip(' \t\n\r')
+        rowDone = rowNoSpaces.replace('\"\"','\"')
+        print(rowDone)
 
-#         whitecardelem = driver.find_element(By.ID, 'AddaWhiteCard')
-#         whitecardelem.send_keys(rowDone)
+        whitecardelem = driver.find_element(By.ID, 'AddaWhiteCard')
+        whitecardelem.send_keys(rowDone)
 
-#         whitecardelem.submit()
+        whitecardelem.submit()
+
+whiteCardFile = input('Enter the path to the black card text file: ')
 
 with open('baiblack.tsv', newline='', encoding="utf8") as csvfile:
 
@@ -112,7 +124,7 @@ with open('baiblack.tsv', newline='', encoding="utf8") as csvfile:
 
         blackcardelem.submit()
 
-
+print('Thanks for using this tool!')
 
 # Close the driver
 driver.quit()
